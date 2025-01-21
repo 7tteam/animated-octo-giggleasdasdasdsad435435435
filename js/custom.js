dbd58927d1831fc981daabed181118e934e7caaf
7t <152796587+7tteam@users.noreply.github.com>
2025-01-21 16:13:48 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,86 @@
+/*
+Template Name: Jarda - ESports, Online Game and Gaming Store HTML Template
+Author: Askbootstrap
+Author URI: https://themeforest.net/user/askbootstrap/portfolio
+Version: 1.0
+*/
+
+$(document).ready(function() {
+    "use strict";
+    
+    
+
+//
+$("body").on("contextmenu",function(e){
+        return false;
+    });
+    $(document).keydown(function(e){
+         if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)){
+           return false;
+         }
+         if(e.which === 123){
+             return false;
+         }
+         if(e.metaKey){
+             return false;
+         }
+         //document.onkeydown = function(e) {
+         // "I" key
+         if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+             return false;
+         }
+         // "J" key
+         if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+             return false;
+         }
+         // "S" key + macOS
+         if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+             return false;
+         }
+         if (e.keyCode == 224 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+             return false;
+         }
+         // "U" key
+         if (e.ctrlKey && e.keyCode == 85) {
+            return false;
+         }
+         // "F12" key
+         if (event.keyCode == 123) {
+            return false;
+         }
+    });
+ 
+	
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','../../../../www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-120909275-1', 'auto');
+ga('send', 'pageview');
+
+     // Add smooth scrolling to all links
+  $(".sidebar-widget .nav-link").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+	
+});
