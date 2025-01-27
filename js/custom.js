2cccfcc2245ad17e30a7f567cf9ea06d13c7eab6
7t <152796587+7tteam@users.noreply.github.com>
2025-01-27 22:54:07 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,86 +1,78 @@
-/*
-Template Name: Jarda - ESports, Online Game and Gaming Store HTML Template
-Author: Askbootstrap
-Author URI: https://themeforest.net/user/askbootstrap/portfolio
-Version: 1.0
-*/
-
 $(document).ready(function() {
     "use strict";
-    
-    
 
-//
-$("body").on("contextmenu",function(e){
-        return false;
+    // حذف تعطيل القوائم المنبثقة (Right-click menu)
+    // $("body").on("contextmenu", function(e) {
+    //     return false;
+    // });
+
+    // حذف تعطيل اختصارات لوحة المفاتيح مثل النسخ واللصق والفحص
+    $(document).keydown(function(e) {
+        // إزالة هذه السطور لتعطيل اختصارات لوحة المفاتيح
+        // if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {
+        //     return false;
+        // }
+        // if(e.which === 123){
+        //     return false;
+        // }
+        // if(e.metaKey){
+        //     return false;
+        // }
+        // if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        //     return false;
+        // }
+        // if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+        //     return false;
+        // }
+        // if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        //     return false;
+        // }
+        // if (e.keyCode == 224 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        //     return false;
+        // }
+        // if (e.ctrlKey && e.keyCode == 85) {
+        //    return false;
+        // }
+        // if (event.keyCode == 123) {
+        //     return false;
+        // }
     });
-    $(document).keydown(function(e){
-         if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)){
-           return false;
-         }
-         if(e.which === 123){
-             return false;
-         }
-         if(e.metaKey){
-             return false;
-         }
-         //document.onkeydown = function(e) {
-         // "I" key
-         if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-             return false;
-         }
-         // "J" key
-         if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-             return false;
-         }
-         // "S" key + macOS
-         if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-             return false;
-         }
-         if (e.keyCode == 224 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-             return false;
-         }
-         // "U" key
-         if (e.ctrlKey && e.keyCode == 85) {
-            return false;
-         }
-         // "F12" key
-         if (event.keyCode == 123) {
-            return false;
-         }
+
+    // تمكين استخدام Google Analytics (لا تغيير هنا)
+    (function(i,s,o,g,r,a,m){
+        i['GoogleAnalyticsObject']=r;
+        i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)
+        },i[r].l=1*new Date();
+        a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];
+        a.async=1;
+        a.src=g;
+        m.parentNode.insertBefore(a,m)
+    })(window,document,'script','../../../../www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-120909275-1', 'auto');
+    ga('send', 'pageview');
+
+    // إضافة التمرير السلس لجميع الروابط
+    $(".sidebar-widget .nav-link").on('click', function(event) {
+        // تأكد من أن هذا.hash يحتوي على قيمة قبل تعديل السلوك الافتراضي
+        if (this.hash !== "") {
+            // منع سلوك النقر الافتراضي
+            event.preventDefault();
+
+            // تخزين الـ hash
+            var hash = this.hash;
+
+            // استخدام jQuery's animate() لإضافة التمرير السلس
+            // الرقم الاختياري (800) يحدد عدد الملي ثانية التي يستغرقها التمرير إلى المنطقة المحددة
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                // إضافة الـ hash (#) إلى URL عند الانتهاء من التمرير
+                window.location.hash = hash;
+            });
+        } // نهاية الـ if
     });
- 
-	
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','../../../../www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-120909275-1', 'auto');
-ga('send', 'pageview');
-
-     // Add smooth scrolling to all links
-  $(".sidebar-widget .nav-link").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-	
 });
